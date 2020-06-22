89de2d86dbf19c158d56d3cd0d767f70a4d55295
Azat Khuzhin <a3at.mail@gmail.com>
2020-06-22 22:16:12 +0300

    Use new queryid annotation in stateless tests
    Only stateless tests:
      gg system.*_log -- tests/queries/0_stateless/*.sql

--- tests/queries/0_stateless/01231_log_queries_min_type.sql
+++ tests/queries/0_stateless/01231_log_queries_min_type.sql
@@ -1,15 +1,15 @@
 set log_queries=1;
 
-select '01231_log_queries_min_type/QUERY_START';
+select 1; -- { queryId 01231_log_queries_min_type-QUERY_START }
 system flush logs;
-select count() from system.query_log where query like '%01231_log_queries_min_type/%' and query not like '%system.query_log%' and event_date = today() and event_time >= now() - interval 1 minute;
+select count() from system.query_log where query_id = '01231_log_queries_min_type-QUERY_START' and event_date = today() and event_time >= now() - interval 1 minute;
 
 set log_queries_min_type='EXCEPTION_BEFORE_START';
-select '01231_log_queries_min_type/EXCEPTION_BEFORE_START';
+select 1; -- { queryId 01231_log_queries_min_type-EXCEPTION_BEFORE_START }
 system flush logs;
-select count() from system.query_log where query like '%01231_log_queries_min_type/%' and query not like '%system.query_log%' and event_date = today() and event_time >= now() - interval 1 minute;
+select count() from system.query_log where query_id = '01231_log_queries_min_type-EXCEPTION_BEFORE_START' and event_date = today() and event_time >= now() - interval 1 minute;
 
 set log_queries_min_type='EXCEPTION_WHILE_PROCESSING';
-select '01231_log_queries_min_type/', max(number) from system.numbers limit 1e6 settings max_rows_to_read='100K'; -- { serverError 158; }
+select max(number) from system.numbers limit 1e6 settings max_rows_to_read='100K'; -- { serverError 158, queryId 01231_log_queries_min_type-EXCEPTION_WHILE_PROCESSING }
 system flush logs;
-select count() from system.query_log where query like '%01231_log_queries_min_type/%' and query not like '%system.query_log%' and event_date = today() and event_time >= now() - interval 1 minute;
+select count() from system.query_log where query_id = '01231_log_queries_min_type-EXCEPTION_WHILE_PROCESSING' and event_date = today() and event_time >= now() - interval 1 minute;
