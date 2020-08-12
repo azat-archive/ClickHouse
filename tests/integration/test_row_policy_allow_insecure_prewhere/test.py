29f936379ea7a6027a17a22c8a787bdaa53fe1b9
Azat Khuzhin <a3at.mail@gmail.com>
2020-08-13 02:34:40 +0300

    Sync test_row_policy_allow_insecure_prewhere

--- tests/integration/test_row_policy_allow_insecure_prewhere/test.py
+++ tests/integration/test_row_policy_allow_insecure_prewhere/test.py
@@ -1,10 +1,14 @@
 # pylint: disable=line-too-long
 
+import re
 import pytest
 from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
 node = cluster.add_instance("node", config_dir="configs")
+
+def strip(s):
+    return re.sub(r'[\s]+', ' ', s.strip())
 
 @pytest.fixture(scope="module", autouse=True)
 def started_cluster():
@@ -32,7 +36,7 @@ def test_PREWHERE():
         'max_threads': 1,
         'optimize_move_to_prewhere': 1,
         # enough to trigger an error if PREWHERE does not works
-        'max_bytes_to_read': 1000 + 10*4,
+        'max_bytes_to_read': 1000 + 30,
     }
 
     # Check that filter does works
@@ -40,13 +44,29 @@ def test_PREWHERE():
     assert int(node.query("SELECT count() FROM mydb.prewhere_filter")) == 1
 
     # PREWHERE
-    node.query("SELECT * FROM mydb.prewhere_no_filter PREWHERE c = 1 AND b = 1 FORMAT Null", settings=settings)
-    node.query("SELECT * FROM mydb.prewhere_filter    PREWHERE c = 1 FORMAT Null", settings=settings)
+    node.query("SELECT * FROM mydb.prewhere_no_filter PREWHERE c = 3 AND b = 3 FORMAT Null", settings=settings)
+    node.query("SELECT * FROM mydb.prewhere_filter    PREWHERE c = 3 FORMAT Null", settings=settings)
 
     # WHERE w/ optimize_move_to_prewhere
-    node.query("SELECT * FROM mydb.prewhere_no_filter WHERE c = 1 AND b = 1 FORMAT Null", settings=settings)
-    node.query("SELECT * FROM mydb.prewhere_filter    WHERE c = 1 FORMAT Null", settings=settings)
+    node.query("SELECT * FROM mydb.prewhere_no_filter WHERE c = 3 AND b = 3 FORMAT Null", settings=settings)
+    node.query("SELECT * FROM mydb.prewhere_filter    WHERE c = 3 FORMAT Null", settings=settings)
+
+    # PREWHERE and WHERE w/ optimize_move_to_prewhere
+    node.query("SELECT s FROM mydb.prewhere_filter PREWHERE 1 WHERE c = 3 FORMAT Null", settings=settings)
+    # EXPLAIN PREWHERE and WHERE w/ optimize_move_to_prewhere
+    assert strip(node.query("EXPLAIN SYNTAX SELECT s FROM mydb.prewhere_no_filter WHERE b = 3 AND 1 = 1 AND c = 3", settings=settings)) == \
+        strip("""
+        SELECT s
+        FROM mydb.prewhere_no_filter
+        PREWHERE (b = 3) AND (c = 3)
+        """)
+    assert strip(node.query("EXPLAIN SYNTAX SELECT s FROM mydb.prewhere_filter WHERE c = 3", settings=settings)) == \
+        strip("""
+        SELECT s
+        FROM mydb.prewhere_filter
+        PREWHERE (b = 3) AND (c = 3)
+        """)
 
     # WHERE w/o optimize_move_to_prewhere (just make sure it works)
-    node.query("SELECT * FROM mydb.prewhere_no_filter WHERE a = 1 AND b = 1 FORMAT Null")
-    node.query("SELECT * FROM mydb.prewhere_filter    WHERE a = 1 FORMAT Null")
+    node.query("SELECT * FROM mydb.prewhere_no_filter WHERE a = 3 AND b = 3 FORMAT Null", settings={'optimize_move_to_prewhere': 0})
+    node.query("SELECT * FROM mydb.prewhere_filter    WHERE a = 3 FORMAT Null", settings={'optimize_move_to_prewhere': 0})
