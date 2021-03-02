da73ae0305f83ae2e3ca352009bbc0a53781d9f2
Azat Khuzhin <a3at.mail@gmail.com>
2021-03-02 23:46:40 +0300

    Fix 00505_shard_secure flakiness
    https://clickhouse-test-reports.s3.yandex.net/21318/38be9ff43ac4c46ce6e803fc125d910bde1d4c71/functional_stateless_tests_(release,_databasereplicated).html#fail1

--- tests/queries/0_stateless/00505_shard_secure.sh
+++ tests/queries/0_stateless/00505_shard_secure.sh
@@ -6,11 +6,13 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT -q "SELECT * FROM remoteSecure('127.0.0.{1,2}', system.one);"
-$CLICKHOUSE_CLIENT -q "SELECT * FROM remoteSecure('127.0.0.{1,2}:$CLICKHOUSE_PORT_TCP_SECURE', system.one);"
-$CLICKHOUSE_CLIENT -q "SELECT * FROM remoteSecure('127.0.0.{1,2}', system.one);"
-$CLICKHOUSE_CLIENT -q "SELECT * FROM remoteSecure(test_shard_localhost_secure, system.one);"
-$CLICKHOUSE_CLIENT -q "SELECT * FROM remote(test_shard_localhost_secure, system.one);"
-$CLICKHOUSE_CLIENT -q "SELECT * FROM remoteSecure(test_shard_localhost, system.one);"
-$CLICKHOUSE_CLIENT -q "SELECT * FROM remote(test_shard_localhost, system.one);"
+# --send_logs_level=fatal to avoid common issue with reconnects:
+# <Warning> ConnectionPoolWithFailover: Connection failed at try №1, reason: Code: 209, e.displayText() = DB::NetException: Timeout
+$CLICKHOUSE_CLIENT --send_logs_level=fatal -q "SELECT * FROM remoteSecure('127.0.0.{1,2}', system.one);"
+$CLICKHOUSE_CLIENT --send_logs_level=fatal -q "SELECT * FROM remoteSecure('127.0.0.{1,2}:$CLICKHOUSE_PORT_TCP_SECURE', system.one);"
+$CLICKHOUSE_CLIENT --send_logs_level=fatal -q "SELECT * FROM remoteSecure('127.0.0.{1,2}', system.one);"
+$CLICKHOUSE_CLIENT --send_logs_level=fatal -q "SELECT * FROM remoteSecure(test_shard_localhost_secure, system.one);"
+$CLICKHOUSE_CLIENT --send_logs_level=fatal -q "SELECT * FROM remote(test_shard_localhost_secure, system.one);"
+$CLICKHOUSE_CLIENT --send_logs_level=fatal -q "SELECT * FROM remoteSecure(test_shard_localhost, system.one);"
+$CLICKHOUSE_CLIENT --send_logs_level=fatal -q "SELECT * FROM remote(test_shard_localhost, system.one);"
 
